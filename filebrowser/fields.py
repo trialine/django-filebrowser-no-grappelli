60a86ad5fcecf57ac3f294efe788b5a8e2b3faa9
smacker <max@smacker.ru>
2019-12-03 20:01:45 +0100

    make Field.from_db_value compatible with Django 3.0

--- filebrowser/fields.py
+++ filebrowser/fields.py
@@ -108,7 +108,7 @@ class FileBrowseField(CharField):
             return value
         return FileObject(value, site=self.site)
 
-    def from_db_value(self, value, expression, connection, context):
+    def from_db_value(self, value, *args, **kwargs):
         return self.to_python(value)
 
     def get_prep_value(self, value):
@@ -241,7 +241,7 @@ class FileBrowseUploadField(CharField):
         self.temp_upload_dir = kwargs.pop('temp_upload_dir', '')
         return super(FileBrowseUploadField, self).__init__(*args, **kwargs)
 
-    def from_db_value(self, value, expression, connection, context):
+    def from_db_value(self, value, *args, **kwargs):
         return self.to_python(value)
 
     def to_python(self, value):
